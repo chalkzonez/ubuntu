6af86a7d69c06a2cdaa1daeee07c2ac6e55d0e2f
chalkzonez <chalkzonezzzz@gmail.com>
2022-12-19 02:31:51 +0000

    [skip ci] import script

---
+++ build.sh
@@ -0,0 +1,23 @@
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/chalkzonez/local_manifest --depth 1 -b nad-10 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source $CIRRUS_WORKING_DIR/script/config
+timeStart
+
+source build/envsetup.sh
+export BUILD_USERNAME=$BUILD_USERNAME
+export BUILD_HOSTNAME=$BUILD_HOSTNAME
+export ALLOW_MISSING_DEPENDENCIES=true
+lunch nad_ginkgo-userdebug
+mkfifo reading # Jangan di Hapus
+tee "${BUILDLOG}" < reading & # Jangan di Hapus
+build_message "Building Started" # Jangan di Hapus
+progress & # Jangan di Hapus
+mka nad -j8  > reading & sleep 95m # Jangan di hapus text line (> reading)
+
+retVal=$?
+timeEnd
+statusBuild
